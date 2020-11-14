eec90d34f3d397e33e2e3e69dd2ed0f2ccad60c8
Ionut Breaz <ibreaz@yahoo.com>
2020-11-14 09:53:00 +0200

    adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class PetType {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public PetType setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
